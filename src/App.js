d2d4ced0bedd104b21ea913ebdcc65791fc9c852
Scott W <scottaza@gmail.com>
2021-08-17 18:56:19 -0700

    Adding basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 
 import './App.css';
 import {HomePage} from './pages/homepage/homepage.component.jsx';
+import {Switch, Route} from 'react-router-dom';
+
+const HatsPage = props => {
+  
+  return (
+  <div>
+    <h1>HATS PAGE: {props.match.params.id}</h1>
+  </div>
+  )
+}
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route  path='/shop/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
