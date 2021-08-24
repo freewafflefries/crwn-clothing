31ffeeb84f3d3d22b1cb735e368632fc06701032
Scott W <scottaza@gmail.com>
2021-08-24 13:13:03 -0400

    Adding Heacer component, Links to Home, Shop & Contact pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@
 import './App.css';
 import {HomePage} from './pages/homepage/homepage.component.jsx';
 import {Switch, Route} from 'react-router-dom';
-import ShopPage from './components/shop/shop.component.jsx'
+import ShopPage from './components/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
 
 
@@ -10,6 +11,7 @@ import ShopPage from './components/shop/shop.component.jsx'
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route  path='/shop' component={ShopPage}/>
