698117f677e2be3146eb336d9c4104708cc0d33d
Scott W <scottaza@gmail.com>
2021-09-04 11:23:59 -0700

    Update some components to use styling wtih javascript

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Directory from '../../components/directory/directory.component.jsx'
-import './homepage.styles.scss'
+//import './homepage.styles.scss'
+import { HomePageContainer } from './homepage.styles.jsx'
 
 export const HomePage = () => (
-    <div className='homepage'>
-     <Directory />
-    </div>
+    <HomePageContainer>
+        <Directory />
+    </HomePageContainer>
  
 
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development'){
     middleware.push(logger)
 }
 
-export const store = createStore(rootReducer, applyMiddleware(middleware))
+export const store = createStore(rootReducer, applyMiddleware(...middleware))
 
 export const persistor = persistStore(store)
 
